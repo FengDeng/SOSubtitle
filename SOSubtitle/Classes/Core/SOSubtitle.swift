1609ebbabe5353c7b627294897a0ffa660ebbfb8
邓锋 <704292743@qq.com>
2018-09-04 15:24:59 +0800

    first commit

---
+++ SOSubtitle/Classes/Core/SOSubtitle.swift
@@ -0,0 +1,18 @@
+//
+//  SOSubtitle.swift
+//  SOSubtitle
+//
+//  Created by 邓锋 on 2018/9/4.
+//  Copyright © 2018年 fengdeng. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class SOSubtitle{
+    
+    var text = ""//文本
+    var color = UIColor.white //颜色
+    var start = 0 //开始显示时间
+    var end = 0 //结束时间
+    
+}
---
+++ SOSubtitle/Classes/Parse/SRTParse.swift
@@ -0,0 +1,9 @@
+//
+//  SRTParse.swift
+//  SOSubtitle
+//
+//  Created by 邓锋 on 2018/9/4.
+//  Copyright © 2018年 fengdeng. All rights reserved.
+//
+
+import Foundation
